60fa7a7460dec38fa5f237efd395a5324004f732
Varun <vs6214125@gmail.com>
2026-09-01 12:53:45 +0530

    first message

---
+++ src/com/corejava/Count.java
@@ -0,0 +1,18 @@
+package com.corejava;
+
+class Student {
+    static int count = 0;
+
+    Student() {
+        count++;
+    }
+
+    public static void main(String[] args) {
+        Student s1 = new Student();
+        Student s2 = new Student();
+        Student s3 = new Student();
+        Student s4 = new Student();
+
+        System.out.println("Number of objects created: " + count);
+    }
+}
